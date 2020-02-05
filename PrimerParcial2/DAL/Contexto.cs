d9dc6df9c65d673245773a30a2aee4decea96ec0
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-02-05 15:59:33 -0500

    Listo

---
+++ PrimerParcial2/DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcial2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Dbset<> {get; set;}
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(@"Server = (local); Database = Prestamos; Trusted_Connection = True;");
+    }
+
+
+}
+
